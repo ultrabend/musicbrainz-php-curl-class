df52d93943f8c5af594b3e91e6b75664f59a43f7
pierre thery <pierre.thery@gmail.com>
2016-10-17 01:13:20 +0200

    add disc search

--- musicbrainz.php
+++ musicbrainz.php
@@ -1,4 +1,5 @@
 <?php
+
 /* MusicBrainz PHP Class for Web Service Version 2
  * Licence: http://musicbrainz.org/doc/Live_Data_Feed
  * API: http://musicbrainz.org/doc/Development/XML_Web_Service/Version_2
@@ -6,7 +7,7 @@
 
 
     public function ReleaseSearch($release, $fmt = 'json') {
-        $url = "http://musicbrainz.org/ws/2/release/?query=release:".urlencode($release)."&fmt=".$fmt;//."&artist=".urlencode($artist)."&fmt=".$fmt);
+        $url = "http://musicbrainz.org/ws/2/release/?query=release:".urlencode($release)."&fmt=".$fmt;
         if ($fmt == 'json') {
           $ch = curl_init();
           curl_setopt($ch, CURLOPT_URL, $url);
@@ -20,6 +21,20 @@
         return $response;
     }
 
+      public function DiscSearch($discid, $fmt = 'json') {
+        $url = "http://musicbrainz.org/ws/2/release/".urlencode($discid)."?inc=recordings&fmt=$fmt";
 
+          if ($fmt == 'json') {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_USERAGENT, 'CdBase');
+            $response = curl_exec ($ch);
+            curl_close($ch);
+            $response = json_decode($response, JSON_FORCE_OBJECT);
+            print_r(error_get_last());
+          }
+          return $response;
+      }
 }
 ?>
